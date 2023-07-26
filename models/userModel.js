6e95b3879b896f561ff9d91d01c42d2069a5dc5c
Diana Kovacheva <dmarinova@outlook.com>
2023-07-26 22:00:57 +0200

    Adds email index to user schema

--- models/userModel.js
+++ models/userModel.js
@@ -99,6 +99,16 @@ userSchema.pre(
   { timestamps: { createdAt: "created_at" } }
 );
 
+userSchema.index(
+  { email: 1 },
+  {
+    collation: {
+      locale: "en",
+      strength: 2,
+    },
+  }
+);
+
 const User = model("User", userSchema);
 
 module.exports = User;
