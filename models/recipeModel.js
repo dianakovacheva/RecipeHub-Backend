cbe41b680aee4eb6b1a347ef76503038b8090f85
Diana Kovacheva <dmarinova@outlook.com>
2023-07-31 18:01:10 +0200

    Updates the recipe model

--- models/recipeModel.js
+++ models/recipeModel.js
@@ -1,11 +1,11 @@
 const { Schema, model, Types } = require("mongoose");
 
-const URL_PATTERN =
-  /(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-/]))?/i;
+const IMAGE_URL_PATTERN =
+  /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|jpeg|gif|png|bmp|svg)/;
 
 const recipeSchema = new Schema(
   {
-    recipeApiID: {
+    recipeApiId: {
       type: String,
       trim: true,
     },
@@ -39,11 +39,11 @@ const recipeSchema = new Schema(
       type: Number,
       min: [0, "The price per serving must be a positive number."],
     },
-    image: {
+    imageURL: {
       type: String,
       required: true,
       validate: {
-        validator: (value) => URL_PATTERN.test(value),
+        validator: (value) => IMAGE_URL_PATTERN.test(value),
         message: "Invalid URL.",
       },
     },
@@ -52,14 +52,13 @@ const recipeSchema = new Schema(
       trim: true,
       required: true,
       minlength: [5, "The summary must be at least 5 characters long."],
-      maxlength: [500, "The summary must be no longer than 500 characters."],
+      maxlength: [1000, "The summary must be no longer than 1000 characters."],
     },
     dishTypes: [
       {
-        type: String,
+        type: [String],
         trim: true,
         required: true,
-        enum: ["lunch", "main course", "main dish", "dinner"],
       },
     ],
     extendedIngredients: [
@@ -80,7 +79,7 @@ const recipeSchema = new Schema(
               required: true,
               min: [0, "The amount must be a positive number."],
             },
-            unitLong: {
+            unitShort: {
               type: String,
               trim: true,
               required: true,
@@ -92,32 +91,30 @@ const recipeSchema = new Schema(
     ],
     analyzedInstructions: [
       {
-        steps: {
-          number: {
-            type: Number,
-            required: true,
-            min: [1, "The number of the steps must begin from 1."],
+        steps: [
+          {
+            number: {
+              type: Number,
+              required: true,
+              min: [1, "The number of the steps must begin from 1."],
+            },
+            step: {
+              type: String,
+              trim: true,
+              required: true,
+              minlength: [
+                2,
+                "The step's description must be at least 2 characters long.",
+              ],
+              // maxlength: [
+              //   1000,
+              //   "The step's description must be no longer than 1000 characters.",
+              // ],
+            },
           },
-          step: {
-            type: String,
-            trim: true,
-            required: true,
-            minlength: [
-              2,
-              "The step's description must be at least 2 characters long.",
-            ],
-            // maxlength: [
-            //   1000,
-            //   "The step's description must be no longer than 1000 characters.",
-            // ],
-          },
-        },
+        ],
       },
     ],
-    recipeOwner: {
-      type: Types.ObjectId,
-      ref: "User",
-    },
     commentsList: {
       type: [Types.ObjectId],
       ref: "Comment",
