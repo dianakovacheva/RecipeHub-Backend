d592fdf4a500922d0a34735f2ca2893e12e370f8
Diana Kovacheva <dmarinova@outlook.com>
2023-07-31 11:33:56 +0200

    Adds recipes route "/recipes"

--- router/index.js
+++ router/index.js
@@ -1,11 +1,10 @@
 const router = require("express").Router();
 const users = require("./users");
 const auth = require("./auth");
+const recipes = require("./recipes");
 
 router.use("/users", users);
 router.use("/auth", auth);
-
-// router.use("/recipes", recipes);
-// router.use("/saved-recipes", saves);
+router.use("/recipes", recipes);
 
 module.exports = router;
