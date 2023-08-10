a763a55b4d5c9fcb35248e151a57848927144a1b
Diana Kovacheva <dmarinova@outlook.com>
2023-08-10 23:26:20 +0200

    Adds recipe and comments related routes

--- router/recipes.js
+++ router/recipes.js
@@ -3,33 +3,39 @@ const router = express.Router();
 const { auth } = require("../utils");
 const { recipeController, commentController } = require("../controllers");
 
-// middleware that is specific to this router
-
-// Recipe router
+// Recipe Related Routes
 router.post("/create-recipe", auth(), recipeController.createRecipe);
 router.put("/details/:recipeId/edit", auth(), recipeController.editRecipe);
 router.put("/details/:recipeId/save", auth(), recipeController.saveRecipe);
+router.delete(
+  "/details/:recipeId/remove-saved",
+  auth(),
+  recipeController.removeSavedRecipe
+);
 router.delete(
   "/details/:recipeId/delete",
   auth(),
   recipeController.deleteRecipe
 );
+// Comment Related Routes
+router.get("/details/:recipeId/comments", recipeController.getRecipeComments);
+router.post(
+  "/details/:recipeId/comment",
+  auth(),
+  commentController.commentRecipe
+);
+// router.put(
+//   "/details/:recipeId/comments/:commentId",
+//   auth(),
+//   commentController.editComment
+// );
+// router.delete(
+//   "/details/:recipeId/comments/:commentId",
+//   auth(),
+//   commentController.deleteComment
+// );
+
 router.get("/details/:recipeId", recipeController.getRecipeById);
-
-// Comment router
-router.post("/details/:recipeId", auth(), commentController.createComment);
-router.put("/details/:recipeId", auth(), commentController.createComment);
-router.put(
-  "/details/:recipeId/comments/:commentId",
-  auth(),
-  commentController.editComment
-);
-router.delete(
-  "/details/:recipeId/comments/:commentId",
-  auth(),
-  commentController.deleteComment
-);
-
 router.get("/", recipeController.getAllRecipes);
 
 module.exports = router;
